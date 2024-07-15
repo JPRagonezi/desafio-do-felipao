c645eae73203fef703a2fed7475e99bb8c8bc4f1
JPRagonezi <jpragonezi@hotmail.com>
2024-07-15 17:17:01 -0300

    variaveis e for

--- big-phillip.js
+++ big-phillip.js
@@ -1,3 +1,9 @@
-let nome = "Supimpa";
-console.log(nome);
+// variaveis 
+let playerName = "Daniel Paladino"
+let playerExperience = 1000
 
+// for
+for (let experienceGained = 0; experienceGained <= playerExperience; experienceGained++){
+    console.log("The player " + playerName + " received " + experienceGained + " Experience points.")
+}
+console.log("Has a total of " + playerExperience + " experience points.");
